522a436b4ce3b7cf04542347bb56e94400b0216b
Diego Muñoz Castro <diego.andresmc@gmail.com>
2023-03-20 18:56:26 -0300

    add config gorm

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/diegoamc92/go-gorm-restapi/db"
 	"github.com/diegoamc92/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db.DBConnection()
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.Home)
 
