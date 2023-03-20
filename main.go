2f661367c58ac5889f64b4d410e3efc301bfbd21
Diego Muñoz Castro <diego.andresmc@gmail.com>
2023-03-20 19:00:53 -0300

    Revert "add config gorm"
    This reverts commit 522a436b4ce3b7cf04542347bb56e94400b0216b.

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"net/http"
 
-	"github.com/diegoamc92/go-gorm-restapi/db"
 	"github.com/diegoamc92/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	db.DBConnection()
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.Home)
 
