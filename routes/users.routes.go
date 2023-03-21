a9643ae9530254eaad0538dd7a4889ade3c0c2fb
Diego Muñoz Castro <diego.andresmc@gmail.com>
2023-03-21 16:54:40 -0300

    config view users

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,15 +1,31 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/diegoamc92/go-gorm-restapi/db"
+	"github.com/diegoamc92/go-gorm-restapi/models"
+	"net/http"
+)
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get Users"))
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
 }
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Get User"))
 }
 func PostUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Post"))
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	createUser := db.DB.Create(&user)
+	err := createUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+	json.NewEncoder(w).Encode(&user)
+
 }
 func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Delete"))
